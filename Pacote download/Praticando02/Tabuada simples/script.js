8cb94e5cab0daa8b3df418abd9a0259f75d0a8cc
Guilherme Vitor Thuler <guilhermevthuler@gmail.com>
2022-11-22 18:05:20 -0300

    Tabuada simples em JS

---
+++ Pacote download/Praticando02/Tabuada simples/script.js
@@ -0,0 +1,30 @@
+var n = prompt('Digite um numero:')
+var res = document.getElementById('res')
+
+res.innerHTML = 
+(`
+
+${n} x 0 = ${n*0}
+<br>
+${n} x 1 = ${n*1}
+<br>
+${n} x 2 = ${n*2}
+<br>
+${n} x 3 = ${n*3}
+<br>
+${n} x 4 = ${n*4}
+<br>
+${n} x 5 = ${n*5}
+<br>
+${n} x 6 = ${n*6}
+<br>
+${n} x 7 = ${n*7}
+<br>
+${n} x 8 = ${n*8}
+<br>
+${n} x 9 = ${n*9}
+<br>
+${n} x 10 = ${n*10}
+
+`) 
+
